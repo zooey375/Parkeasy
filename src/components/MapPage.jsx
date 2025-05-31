527aecae96cc966e16fe8b583bbca46a79042d46
unknown <yu.ting375@gmail.com>
2025-05-31 18:13:33 +0800

    引入 Marker 這個元件

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,8 +1,10 @@
 import Sidebar from './Sidebar';
 import { useState, useEffect } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'; // 本來一連上後端畫面就會閃白，結果加了Marker, Popup 畫面就成功了!
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -16,7 +18,17 @@ function MapPage() {
   const [filters, setFilters] = useState({ type: '', friendly: '', price: '' });
   const [parkingLots, setParkingLots] = useState([]);
 
+ // 自訂地圖圖示放在這裡
+  const customIcon = new L.Icon({
+    iconUrl: '/images/parking-icon.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
+
+
   useEffect(() => {
+    // 抓資料...
     const query = new URLSearchParams();
     if (filters.type) query.append('type', filters.type);
     if (filters.friendly) query.append('friendly', filters.friendly);
@@ -28,7 +40,9 @@ function MapPage() {
 
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setParkingLots(data));
+      .then((data) => setParkingLots(data))
+      .catch((err) => console.error('資料仔入失敗:', err)); //即使資料庫斷線也不會報錯
+
   }, [filters]);
 
 
@@ -47,7 +61,10 @@ function MapPage() {
           />
 
           {parkingLots.map(lot => (
-            <Marker key={lot.id} position={[lot.latitude, lot.longitude]}>
+            <Marker key={lot.id} 
+            position={[lot.latitude, lot.longitude]}  // 使用經緯度來設定標記位置
+            icon={ customIcon } // 使用自訂圖示(map 圖示)
+            >
               <Popup>
                 <strong>{lot.name}</strong><br />
                 類型：{lot.type}<br />
